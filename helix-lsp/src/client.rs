d182ba5d14189dc32eef21463c8c6e3aed83325e
MegaBluejay <dmoiseev2011@gmail.com>
2024-09-26 03:47:37 -0400

    add support for localDocs

--- helix-lsp/src/client.rs
+++ helix-lsp/src/client.rs
@@ -720,6 +720,9 @@ impl Client {
                     ]),
                     ..Default::default()
                 }),
+                experimental: Some(lsp::ExperimentalClientCapabilities {
+                    local_docs: Some(true),
+                }),
                 ..Default::default()
             },
             trace: None,
@@ -1192,7 +1195,7 @@ impl Client {
         &self,
         text_document: lsp::TextDocumentIdentifier,
         position: lsp::Position,
-    ) -> Option<impl Future<Output = Result<Option<Url>>>> {
+    ) -> Option<impl Future<Output = Result<lsp::ExternalDocsResponse>>> {
         let capabilities = self.capabilities.get().unwrap();
 
         if !capabilities
