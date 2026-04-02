d3162637b665ac0938ec12c96602b7ca071f302e
Rolo <RoloEdits@gmail.com>
2026-04-02 15:52:58 -0700

    feat: add breadcrumb handling

--- helix-view/src/document.rs
+++ helix-view/src/document.rs
@@ -162,6 +162,9 @@ pub struct Document {
     // Stores all the symbols of the Document.
     pub(crate) symbols: Option<DocumentSymbolCache>,
 
+    /// Breadcrumb trail for each view showing this document.
+    pub breadcrumbs: HashMap<ViewId, Breadcrumb>,
+
     path: Option<PathBuf>,
     relative_path: OnceCell<Option<PathBuf>>,
     /// Lazily-computed workspace root for this document (the ancestor that contains a `.git` /
@@ -249,6 +252,54 @@ pub struct Document {
 pub struct DocumentSymbolCache {
     pub tree: Vec<DocumentSymbol>,
     pub offset_encoding: OffsetEncoding,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Breadcrumb(Vec<ThinDocumentSymbol>);
+
+impl Breadcrumb {
+    #[inline]
+    pub fn push(&mut self, symbol: &DocumentSymbol) {
+        self.0.push(ThinDocumentSymbol::from(symbol));
+    }
+
+    #[inline]
+    pub fn clear(&mut self) {
+        self.0.clear();
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn depth(&self) -> usize {
+        self.0.len()
+    }
+
+    #[inline]
+    pub fn iter(&self) -> impl Iterator<Item = &ThinDocumentSymbol> {
+        self.0.iter()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ThinDocumentSymbol {
+    pub name: String,
+    pub kind: lsp::SymbolKind,
+}
+
+impl From<&DocumentSymbol> for ThinDocumentSymbol {
+    #[inline]
+    fn from(symbol: &DocumentSymbol) -> Self {
+        Self {
+            name: symbol.name.clone(),
+            kind: symbol.kind,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Default)]
@@ -795,6 +846,7 @@ impl Document {
             document_link_controller: TaskController::new(),
             symbols: None,
             symbols_controller: TaskController::new(),
+            breadcrumbs: HashMap::new(),
         }
     }
 
@@ -2527,6 +2579,50 @@ impl Document {
 
     pub fn clear_document_symbols(&mut self) {
         self.symbols = None;
+    }
+
+    pub fn update_breadcrumbs_for_view(&mut self, view_id: ViewId) {
+        #[inline(always)]
+        const fn position_is_in_range(pos: lsp::Position, range: lsp::Range) -> bool {
+            if pos.line < range.start.line || pos.line > range.end.line {
+                return false;
+            }
+            if pos.line == range.start.line && pos.character < range.start.character {
+                return false;
+            }
+            if pos.line == range.end.line && pos.character > range.end.character {
+                return false;
+            }
+            true
+        }
+
+        let Some(symbols) = self.symbols.as_ref() else {
+            self.breadcrumbs.remove(&view_id);
+            return;
+        };
+
+        let position = self.position(view_id, symbols.offset_encoding);
+
+        let breadcrumb = self.breadcrumbs.entry(view_id).or_default();
+
+        breadcrumb.clear();
+
+        let mut current = symbols.tree.as_slice();
+
+        while let Some(symbol) = current
+            .iter()
+            .find(|symbol| position_is_in_range(position, symbol.range))
+        {
+            breadcrumb.push(symbol);
+            match symbol.children.as_deref() {
+                Some(children) => current = children,
+                _ => break,
+            }
+        }
+
+        if breadcrumb.is_empty() {
+            self.breadcrumbs.remove(&view_id);
+        }
     }
 
     /// Get the inlay hints for this document and `view_id`.
